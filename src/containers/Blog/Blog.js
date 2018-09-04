29654baa1998ac6cef593f4cbbea568f8f4d6075
the-markussohn <markus.justinas@gmail.com>
2018-09-04 23:14:30 +0300

    display mock posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,29 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state = {
+        posts: []
+    };
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.setState({posts: response.data});
+            })
+            .catch();
+    }
+
     render () {
+        const posts = this.state.posts
+            .map(post => {
+                return <Post key={post.id} title={post.title}/>
+            });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
