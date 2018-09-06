9ef0914275de5db5e29a214771c445bcc297b0f2
the-markussohn <markus.justinas@gmail.com>
2018-09-07 00:32:25 +0300

    navlink and activeStyle

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import {Route, Link} from 'react-router-dom';
+import React, {Component} from 'react';
+import {Route, NavLink} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -7,18 +7,24 @@ import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
-    render () {
+    render() {
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink to="/"
+                                         exact
+                                         activeClassName="active"
+                                         activeStyle={{
+                                             color: '#fa923f',
+                                             textDecoration: 'underline'
+                                         }}>Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: "/new-post",
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
